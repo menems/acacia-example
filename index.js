cbb962a4ec8d037094a4d0f6a8c19e1a17ca4ae2
blaz <blaz@menems.net>
2015-11-20 15:46:35 +0100

    new signature

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const Acacia = require('acacia');
-const config = require('config-helper')();
+
+const conf = {
+    config: {
+        path: 'config/'
+    },
+    services: {
+        path: 'services'
+    },
+    resources: {
+        path: 'resources'
+    }
+}
+
+
+const config = require('config-helper')(conf.config);
 
 const app = new Acacia(config);
 
 app
-    .initServices()
-    .initResources()
+    .services(conf.services)
+    .resources(conf.resources)
     .listen();

513ed613e38929e54beaea8eaa297d094fe76cd6
blaz <blaz@menems.net>
2015-11-20 16:00:15 +0100

    fix: path on config no more usefull

--- config/development.js
+++ config/development.js
@@ -6,9 +6,5 @@ const root = path.resolve(__dirname ,'../');
 
 module.exports = {
     name: 'Acacia example',
-    port : 4661,
-    path : {
-        services : root + '/services',
-        resources : root + '/resources'
-    }
+    port : 4661
 }

d6921c9e4b5f905147eb6584f2f07886fe60191f
blaz <blaz@menems.net>
2015-12-02 14:01:17 +0100

    acacia v0.3.0

--- resources/version/index.js
+++ resources/version/index.js
@@ -3,4 +3,4 @@
 const Router = require('koa-66');
 const router = module.exports = new Router();
 
-router.get('/', ctx => ctx.body = ctx.services.get('version').getVersion());
+router.get('/', ctx => ctx.body = ctx.service('version').getVersion());
